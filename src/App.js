658c375146f44dc2ac15cba7a5603cd1f391241f
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2024-06-08 10:12:11 +0500

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
